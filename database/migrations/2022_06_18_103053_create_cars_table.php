dbf1c195ec3f5288f2d9d2aaee6eabaf50f06267
Alessandro Giunta <alegiunt99@gmail.com>
2022-06-18 12:51:16 +0200

    Create cars table and Car Model

---
+++ database/migrations/2022_06_18_103053_create_cars_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('brand');
+            $table->string('model');
+            $table->float('price', 11, 2);
+            $table->year('year');
+            $table->string('displacement');
+            $table->string('gear');
+            $table->string('license_plate');
+            $table->string('alimentation');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
